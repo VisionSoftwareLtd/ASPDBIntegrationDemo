7d7d1eb45c4e92b8e81534cdc222f3d9f291774c
Dave Wright <dave.wright@unicard-uk.com>
2024-10-18 09:19:58 +0100

    Add database integration

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,26 +6,35 @@ namespace ASPDBIntegrationDemo.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
+  private readonly ILogger<HomeController> _logger;
+  private readonly IConfiguration _configuration;
 
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
+  public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+  {
+    _logger = logger;
+    _configuration = configuration;
+  }
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+  public IActionResult Index()
+  {
+    Database.Instance.SetConnectionString(_configuration.GetValue<string>("ConnectionString"));
+    return View(Database.Instance.GetItems());
+  }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+  public IActionResult Privacy()
+  {
+    return View();
+  }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
+  public IActionResult CreateItemResult(Item item)
+  {
+    Database.Instance.AddItem(item);
+    return View(item);
+  }
+
+  [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+  public IActionResult Error()
+  {
+    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+  }
 }
